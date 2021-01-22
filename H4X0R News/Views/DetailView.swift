18283d2ba0cf109555c20f220c3d865433139af5
47lakers <iraporchia22@gmail.com>
2021-01-21 18:07:49 -0800

    Initial Commit

---
+++ H4X0R News/Views/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Ira Xavier Porchia on 1/9/21.
+//
+
+import SwiftUI
+import WebKit
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
